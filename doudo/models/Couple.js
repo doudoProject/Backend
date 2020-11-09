271822acb967097458d17f25c0fc33bef19198ed
박성철 <gorae02@gmail.com>
2020-11-09 14:57:35 +0000

    Change Group Id -> Couple Id Create Couple Model

--- doudo/models/Couple.js
+++ doudo/models/Couple.js
@@ -2,28 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	userid: {
-        type: String,
-        required: true
-    },
-	email: {
-        type: String,
-        required: true
-    },
-    password: {
+	coupleid: {
         type: String,
         required: true
     },
     name : {
         type: String,
+		default: '커플 이름을 설정해주세요',
         required: true
     },
-	coupleId : {
-        type: String
-    },
-	permission:{
-		type: Number,
-		default:0
+	member:[String],
+	todo:[String],
+	events:{
+		name:{type:String},
+		datetime:{type:String},
+		author:{type:String},
 	}
 });
 
--- doudo/routes/api/couple/index.js
+++ doudo/routes/api/couple/index.js
@@ -1,7 +1,11 @@
 var router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-	res.status(200).send('<h1 align="center">Auth API</h1>');
+/* 
+	POST Sign Up
+	/v1/couple/create
+*/
+router.get('/create', (req, res, next) => {
+	
 });
 
 module.exports = router
