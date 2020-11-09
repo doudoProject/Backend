51851ca2353c386beb30a13d49190f2c6f7511b2
박성철 <gorae02@gmail.com>
2020-11-09 14:46:59 +0000

    Delete API Main Page

--- doudo/routes/api/index.js
+++ doudo/routes/api/index.js
@@ -3,11 +3,6 @@ var auth = require('./auth')
 var couple = require('./couple')
 const passport = require('passport')
 
-/* api Main */
-router.get('/', (req, res, next) => {
-	res.status(200).send('<h1 align="center">API Main</h1>')
-});
-
 router.use('/auth', auth)
 router.use('/couple',passport.authenticate('jwt', { session: false}), couple)
 
